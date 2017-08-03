f2800112574fc8ed025a091e99617a16c70d684d
Kian Davoudi-Rad <davoudi.rk@gmail.com>
2017-08-03 12:29:29 -0700

    adding new structor on the Dev branch

---
+++ NNDCheckIn/NNDCheckIn/Controllers/NNDCheckInController.h
@@ -0,0 +1,22 @@
+
+/*  Copyright © 2017 Kian Davoudi-Rad. All rights reserved.
+ Author: Kian D.Rad
+ Date:   August 3rd 2017
+ ReadMe:
+ */
+#import <UIKit/UIKit.h>
+
+//! Project version number for NNDCheckIn.
+FOUNDATION_EXPORT double NNDCheckInVersionNumber;
+
+//! Project version string for NNDCheckIn.
+FOUNDATION_EXPORT const unsigned char NNDCheckInVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NNDCheckIn/PublicHeader.h>
+
+
+@interface NNDCheckInController : NSObject
+
+-(void) HelloDepenecyIntegrationNNDCheckInController;
+
+@end
